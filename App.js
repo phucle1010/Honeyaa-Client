5910b6c6d8eb99b3a6fd99bf36923d57a37b45a1
Duc47 <nvd501150@gmail.com>
2023-05-08 22:12:47 +0700

    f

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import RestoreAccount from './screens/RestoreAccount';
+import VerifyOTP from './screens/VerifyOTP';
+import VerifyPhoneNumber from './screens/VerifyPhoneNumber';
 import BottomTab from './components/BottomTab';
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-    return (
-        <NavigationContainer>
-            <BottomTab />
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='BottomTab' screenOptions={{headerShown:false}}>
+        <Stack.Screen name="RestoreAccount" component={RestoreAccount} />
+        <Stack.Screen name="VerifyOTP" component={VerifyOTP} />
+        <Stack.Screen name="BottomTab" component={BottomTab} />
+        <Stack.Screen name="VerifyPhoneNumber" component={VerifyPhoneNumber} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
-export default App;
+export default App
