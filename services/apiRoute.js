5c9fbd1a437b873f8573818cdde935fa081b37ee
Annoy1010 <lethephuc2002@gmail.com>
2023-07-15 07:55:39 +0700

    change-root-api

--- services/apiRoute.js
+++ services/apiRoute.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://192.168.1.186:8080';
-// const API_URL = 'https://honeyaa-server.vercel.app';
+// const API_URL = 'http://192.168.1.186:8080';
+const API_URL = 'https://honeyaa-server.vercel.app';
 
 export default API_URL;
